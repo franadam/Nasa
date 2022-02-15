9eeec5083c3afc6ecdefbe05afb3149a563b44f7
francois <francoisadam@hotmail.be>
2022-02-15 11:06:30 +0100

    build(npm): add new npm scripts and add build static files for the server

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,22 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetRouter = require('./routes/planet.router');
 
 const app = express();
 
-// extra package
+// security package
 app.use(
   cors({
     origin: 'http://localhost:3000/',
   })
 );
+app.use(morgan('combined'));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 // routes
 app.use('/api/v1/planets', planetRouter);
